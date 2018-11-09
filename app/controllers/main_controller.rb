a2ef1cded76a98414fde3358f644400c9f3b6a3e
danielpinoga <daniel.pino@generalassemb.ly>
2018-11-08 19:54:05 -0500

    fighting with API. Able to get People data.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,19 @@
 class MainController < ApplicationController
+  require 'HTTParty'
+
   def index
     render text: "", layout: "application"
+  end
+
+  def getPeople
+    base_url = ENV['SALESLOFT_BASE_URL']
+    headers = {
+      "Authorization" => "Bearer #{ENV['SALESLOFT_API_KEY']}"
+    }
+    response = HTTParty.get( base_url + '/v2/people.json', 
+    :headers => headers)
+
+    puts response['data']
+    render json: {'something': 'here'}
   end
 end
