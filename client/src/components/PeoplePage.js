b812436974e7c61d18ecc5ed5f02dbd89f9ca7f5
danielpinoga <daniel.pino@generalassemb.ly>
2018-11-08 21:02:53 -0500

    light formatting. people rendering

--- client/src/components/PeoplePage.js
+++ client/src/components/PeoplePage.js
@@ -1,6 +1,23 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import axios from 'axios'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+
+const StyledPerson = styled(Link)`
+  border: solid 2px black;
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  width: 350px;
+  text-decoration: none;
+`
+const StyledPeopleContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: left;
+  padding: 2px 16px;
+
+`
+
 
 class PeoplePage extends Component {
   constructor(props) {
@@ -14,27 +31,26 @@ class PeoplePage extends Component {
   componentDidMount() {
     axios.get('/api/people')
       .then(response => {
-        console.log('got it')
         this.setState({ people: response.data })
       })
   }
 
 
   render() {
-    console.log('render')
     const peopleContent = this.state.people.map(person => {
       return (
-        <div key={person.id}>
-          Name = {person.first_name}
-        </div>
+        <StyledPerson key={person.id} to={`/people/${person.id}`}>
+          <div>Name: {person.first_name} {person.last_name}</div>
+          <div>Email: {person.email_address}</div>
+        </StyledPerson>
       )
     })
 
     return (
-      <div>
+      <StyledPeopleContainer>
         People PAge
         {peopleContent}
-      </div>
+      </StyledPeopleContainer>
     )
   }
 }
