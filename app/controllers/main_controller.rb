2f6d5c752d0dc1e1dbb4e33df8c3c9245f0f5b67
danielpinoga <daniel.pino@generalassemb.ly>
2018-11-08 20:13:15 -0500

    cleaning out test code

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,16 +4,4 @@ class MainController < ApplicationController
   def index
     render text: "", layout: "application"
   end
-
-  def getPeople
-    base_url = ENV['SALESLOFT_BASE_URL']
-    headers = {
-      "Authorization" => "Bearer #{ENV['SALESLOFT_API_KEY']}"
-    }
-    response = HTTParty.get( base_url + '/v2/people.json', 
-    :headers => headers)
-
-    puts response['data']
-    render json: {'something': 'here'}
-  end
 end
