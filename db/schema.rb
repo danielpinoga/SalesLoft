5dff0de371e24b068e03245b8dbadceba583e9ac
danielpinoga <daniel.pino@generalassemb.ly>
2018-11-09 16:57:25 -0500

    swapping to postgres for heroku deployment

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180114224612) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tokens", force: :cascade do |t|
     t.string "access_token"
     t.string "refresh_token"
