243ad79bcdb2a9b836e99fa72212b8dddcbfa3ca
danielpinoga <daniel.pino@generalassemb.ly>
2018-11-10 19:18:06 -0500

    dupe analysis is working. fighting with infinite loops and need to take a break.

--- client/src/components/PeoplePage.js
+++ client/src/components/PeoplePage.js
@@ -54,17 +54,19 @@ class PeoplePage extends Component {
   render() {
     let peopleContent = Object.keys(this.props.currentPeople).map(key => {
       const person = this.props.currentPeople[key]
-      let emailShard = ''
+      let dupeContent = 'Checking'
       if (this.state.checkForDupes) {
-        emailShard = shardSingleEmail(person.email_address)
+        console.log('doing something')
+        console.log(checkForDupeLogic(person.email_address, this.props.emailShards))
+        dupeContent = 'checked'
       }
-      console.log(emailShard)
+      console.log(dupeContent)
       return (
         <StyledPerson key={key} to={`/people/${person.id}`}>
           <div>Name: {person.first_name} {person.last_name}</div>
           <div>Email: {person.email_address}</div>
           <div>Job Title: {person.title}</div>
-          {this.state.checkForDupes ? <div>CHECKED</div> : null}
+          {this.state.checkForDupes ? <div>{dupeContent}</div> : null}
         </StyledPerson>
       )
     })
@@ -96,7 +98,8 @@ class PeoplePage extends Component {
 const mapStateToProps = (state) => {
   return {
     allPeople: state.peopleInfo.allPeople,
-    currentPeople: state.peopleInfo.currentPeople
+    currentPeople: state.peopleInfo.currentPeople,
+    emailShards: state.peopleInfo.emailShards
   }
 }
 
@@ -106,3 +109,63 @@ const mapDispatchToProps = {
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(PeoplePage)
+
+
+
+const breakEmailIntoSubStrings = (email, subLength, subStrings = {}) => {
+  for (let i = 0; i < email.length - subLength + 1; i++) {
+    const subString = email.substr(i, subLength)
+    if (!subStrings[subString]) subStrings[subString] = { [email]: 0 }
+    subStrings[subString][email] ? subStrings[subString][email] += 1 : subStrings[subString][email] = 1
+  }
+  return subStrings
+}
+
+const subLengthFactor = (subLength) => {
+  return subLength / 2
+}
+
+/*
+Find all substrings within the given email and count their frequency
+Iterate through the substrings found in step 1
+  If the current substring is in the tracking object
+    Iterate through all emails that also contain that substring
+      update the potentialMatch tracking object for that email
+        add the minimum count between (matches in email) and (matches in new email)
+        multiply by the subLength factor
+*/
+const checkNewEmail = (newEmail, subLength, allEmailAnalysis) => {
+  const dupeTracker = {}
+  const newEmailAnalysis = breakEmailIntoSubStrings(newEmail, subLength)
+  const allFoundSubstrings = Object.keys(newEmailAnalysis)
+  allFoundSubstrings.forEach(subString => {
+    if (allEmailAnalysis[subString]) {
+      const emailsWithSubString = Object.keys(allEmailAnalysis[subString])
+      emailsWithSubString.forEach(email => {
+        const hitsInAllEmails = allEmailAnalysis[subString][email]
+        const hitsInNewEmail = newEmailAnalysis[subString][newEmail]
+        const points = Math.min(hitsInAllEmails, hitsInNewEmail) * subLengthFactor(subLength)
+        if (dupeTracker[email]) dupeTracker[email] += points
+        else dupeTracker[email] = points
+      })
+    }
+  })
+  return dupeTracker
+}
+
+const checkForDupeLogic = (newEmail, allEmailShards) => {
+  const dupeCheckResults = {}
+  for (let i = 1; i < newEmail.length; i++) {
+    const allEmailAnalysis = allEmailShards[i]
+    const newEmailAnalysis = checkNewEmail(newEmail, i, allEmailAnalysis)
+    Object.keys(newEmailAnalysis).reduce((finalAnalysis, matchedEmail) => {
+      if (finalAnalysis[matchedEmail]) {
+        finalAnalysis[matchedEmail] += newEmailAnalysis[matchedEmail]
+      } else {
+        finalAnalysis[matchedEmail] = newEmailAnalysis[matchedEmail]
+      }
+      return finalAnalysis
+    }, dupeCheckResults)
+  }
+  return dupeCheckResults
+}
